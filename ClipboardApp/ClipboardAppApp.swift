13956f4e9745afe07db9863cc4cd67a4f69cf1dd
Hellyson-Ferreira <hellysonnascimentolima@gmail.com>
2026-04-07 13:05:07 -0300

    chore: initial Xcode project setup

---
+++ ClipboardApp/ClipboardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardAppApp.swift
+//  ClipboardApp
+//
+//  Created by Hellyson on 07/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
